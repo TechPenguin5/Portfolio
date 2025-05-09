eca50f42bc46260182261e1bd341e0f1e99e45f1
Malindra Perera <malindra@Malindras-MacBook-Pro.local>
2025-05-08 21:06:54 -0600

    Add technologies tooltip

---
+++ app/(landing)/components/TooltipPreview.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import { AnimatedTooltip } from "@/app/components/ui/animated-tooltip";
+const people = [
+  {
+    id: 1,
+    name: "Javascript",
+    designation: "Fullstack",
+    image:
+      "/images/tooltips/js-logo.png",
+  },
+  {
+    id: 2,
+    name: ".NET",
+    designation: "Core/Framework",
+    image:
+      "/images/tooltips/dotnet-logo.png",
+  },
+  {
+    id: 3,
+    name: "SAP",
+    designation: "Techno Functional Consultant",
+    image:
+      "/images/tooltips/sap-logo.png",
+  },
+  {
+    id: 4,
+    name: "SQL",
+    designation: "MySQL/PostgreSQL",
+    image:
+    "/images/tooltips/sql-logo.png",  
+  },
+  {
+    id: 5,
+    name: "Tailwind",
+    designation: "CSS3/SCSS",
+    image:
+      "/images/tooltips/tailwind-logo.png",
+  },
+  {
+    id: 6,
+    name: "Cross Platform Development",
+    designation: "React Native, Flutter & Electron",
+    image:
+    "/images/tooltips/crossplatform-logo.png",    },
+];
+
+export function ToolTipPreview() {
+  return (
+    <div className="flex flex-row items-center justify-center mb-10 w-full">
+      <AnimatedTooltip items={people} />
+    </div>
+  );
+}
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { SpotlightPreview } from "./components/SpotlightPreview";
+import { ToolTipPreview } from "./components/TooltipPreview";
 
 export default function Home() {
   return (
     <div>
       <SpotlightPreview />
+      <ToolTipPreview />
     </div>
   );
 }
