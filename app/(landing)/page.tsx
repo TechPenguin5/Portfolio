4fe1619b1c735cbf4afe407976f3ace6db0210de
Malindra Perera <malindra@Malindras-MacBook-Pro.local>
2025-05-08 20:29:20 -0600

    Create spotlight section for landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { SpotlightPreview } from "./components/SpotlightPreview";
+
+export default function Home() {
+  return (
+    <div>
+      <SpotlightPreview />
+    </div>
+  );
+}
