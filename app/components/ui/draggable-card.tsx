2399973e6aeefd53432e8350b07bcd95997139d3
Malindra Perera <malindra@Malindras-MacBook-Pro.local>
2025-05-12 08:23:50 -0600

    Fix margins

--- app/components/ui/draggable-card.tsx
+++ app/components/ui/draggable-card.tsx
@@ -186,7 +186,7 @@ export const DraggableCardContainer = ({
   children?: React.ReactNode;
 }) => {
   return (
-    <div className="max-w-7xl mx-auto py-2 px-4 md:px-8 lg:px-10">
+    <div className="max-w-7xl mx-auto mb-15 px-4 md:px-8 lg:px-10">
     <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
       Not Just a Developer
     </h2>
